4109c8d3476ee6b4cc4a33dfeb7b48e0d5ed46a6
Oscar <21753725a@iespoblenou.org>
2017-02-14 20:37:32 +0100

    Added camera option

--- app/src/main/java/com/example/a21753725a/todo/MainActivity.java
+++ app/src/main/java/com/example/a21753725a/todo/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
     @Override
@@ -40,4 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/example/a21753725a/todo/MainActivityFragment.java
+++ app/src/main/java/com/example/a21753725a/todo/MainActivityFragment.java
@@ -1,7 +1,13 @@
 package com.example.a21753725a.todo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,49 +18,86 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
-
+    String mCurrentPhotoPath;
     ArrayAdapter adapter;
     View view;
     ArrayList todo;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_main, container, false);
+        view = inflater.inflate(R.layout.fragment_main, container);
         todo = new ArrayList<String>();
 
-        Button btn = (Button) view.findViewById(R.id.button);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
 
-        final EditText editText = (EditText) view.findViewById(R.id.editText);
         ListView lv = (ListView) view.findViewById(R.id.todolst);
         adapter = new ArrayAdapter<String>(this.getContext(),R.layout.list_view_adapter,R.id.listViewText,todo);
 
         lv.setAdapter(adapter);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                adapter.add(String.valueOf(editText.getText()));
-                editText.setText(null);
+                dispatchTakePictureIntent();
             }
         });
-        // Write a message to the database
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //DatabaseReference myRef = database.getReference("message");
-        //myRef.setValue("Hello, World!");
 
         return view;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile.exists()) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
